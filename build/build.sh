bfada1d7438739995f7aceb635c2f58cc5414751
nuanxinqing <nuanxinqing@gmail.com>
2022-11-13 13:53:15 +0800

    更新 配置文件配置项

--- build/build.sh
+++ build/build.sh
@@ -1,6 +1,2 @@
-xgo -out QLToolsPro_1.0 --targets=windows/*,linux/* ../
-rm -rf QLToolsPro_1.0-linux-riscv64
-rm -rf QLToolsPro_1.0-linux-s390x
-rm -rf QLToolsPro_1.0-linux-mips64
-rm -rf QLToolsPro_1.0-linux-mips64le
-upx QLToolsPro_1.0-*
+xgo -out QLToolsPro-1.0 --targets=windows/*,linux/* ../
+upx QLToolsPro-1.0-*
