933183411f6a92ec4c707909d29039b9f63a1f37
nuanxinqing <nuanxinqing@gmail.com>
2022-11-27 22:38:43 +0800

    新增 Web端直接操作系统（关闭或重启）

--- build/build.sh
+++ build/build.sh
@@ -1,3 +1,3 @@
 # xgo -out QLToolsPro --targets=linux/amd64 .
-xgo -out QLToolsPro --targets=windows/*,linux/* ../
+xgo -out QLToolsPro --targets=windows/*,linux/amd64,linux/arm64,linux/amd64 ../
 upx QLToolsPro-*
--- static/assets/js/store-8e5b6e13.js
+++
@@ -1,1 +0,0 @@
-import{u as o}from"./index-5edb5878.js";const s=o();export{s as c};
